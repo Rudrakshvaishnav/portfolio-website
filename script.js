18f149af4cc1b1decc08830ad8803cff9cbd56ec
Rudrakshvaishnav <90573895+Rudrakshvaishnav@users.noreply.github.com>
2024-11-19 00:36:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript to add smooth scrolling effect
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
